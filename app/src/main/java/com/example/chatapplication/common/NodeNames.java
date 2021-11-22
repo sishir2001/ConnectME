1054c5ce90736c21796fef4afd2de026b780d4ff
Jaladi Saai Sishir <54372985+sishir2001@users.noreply.github.com>
2021-11-23 03:19:47 +0530

    SignUp Activity done

---
+++ app/src/main/java/com/example/chatapplication/common/NodeNames.java
@@ -0,0 +1,10 @@
+package com.example.chatapplication.common;
+
+public class NodeNames {
+    public static final String USERS = "users";
+    public static final String EMAIL = "email";
+    public static final String NAME = "name";
+    public static final String PHOTO = "photo";
+    public static final String ONLINE = "online";
+    public static final String IMAGES = "images";
+}
